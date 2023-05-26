9a2aa1ae55a7bd82fa315265da6d255a7a864a80
Arnab <arnab.mondal@selise.com>
2023-05-26 13:07:08 +0600

    initial style for banner block

---
+++ build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => 'f902293ebff07efc1ba8');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '56ddb211d0ef6a07efbe');
